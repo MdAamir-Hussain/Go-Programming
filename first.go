b81d1d1462576ba1434736ea4ef5e1cb40c7c135
MdAamir-Hussain <mdaamirhussain319@gmail.com>
2024-04-30 20:48:28 +0530

    binarySearch

--- first.go
+++ first.go
@@ -4,6 +4,34 @@ import (
 	"fmt"
 )
 
+func binarySearch(arr []int, target int) int {
+	low := 0
+	high := len(arr) - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1 // Element not found
+}
+
 func main() {
-	fmt.Println("Hello go programming in vs code!!!")
+	// fmt.Println("Hello go programming in vs code!!!")
+	arr := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29}
+	target := 13
+
+	index := binarySearch(arr, target)
+	if index != -1 {
+		fmt.Printf("Element %d found at index %d\n", target, index)
+	} else {
+		fmt.Printf("Element %d not found in the array\n", target)
+	}
 }
